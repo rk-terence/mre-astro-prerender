70e254eb92671149a6401530d0013e671a736c1a
rkzhai <rkzhai7@gmail.com>
2023-08-31 21:32:31 +0800

    init

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+
+import node from "@astrojs/node";
+
+// https://astro.build/config
+export default defineConfig({
+  // Enable React to support React JSX components.
+  integrations: [react()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+});
